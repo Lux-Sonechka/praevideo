f94fc42dcd08b6e1c43cf99897eef14e677bb2a8
Lux Sonechka <lux.sonechka@gmail.com>
2024-09-20 23:34:42 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -49,19 +49,27 @@ function Red_Slogan() {
 
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+
 
 // When the user clicks on <span> (x), close the modal
 function close_popup() {
   modal.style.display = "none";
   document.getElementById('song-unfamiliar').play()  
 }
+function close_chapter() {
+    document.getElementById('Modal-chapter').style.display = "none";
+  }
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+}
+function open_chapter() {
+    document.getElementById('Modal-chapter').style.display = "block";
+}
+function chapter_1() {
+    window.location.href = "Chapters/1st/chapter1.html"
 }
