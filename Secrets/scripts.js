aff7e95d2046f9ad7265dd76161d1089b9442993
Lux Sonechka <lux.sonechka@gmail.com>
2024-12-22 16:52:02 -0300

    Add files via upload

---
+++ Secrets/scripts.js
@@ -0,0 +1,33 @@
+// To obfuscate
+
+document.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      
+      var input_password = document.getElementById('password-input')
+      if (input_password.value == '1048') {
+        
+        window.open('https://drive.google.com/file/d/1PbIZ4bwxx8pbnHvREd8wvpWJiynIFMPW/view')
+      }
+      if (input_password.value == 'cat') {
+        document.getElementById('secret-note').style.display = 'block';
+        document.body.style.backgroundImage = "url('silly-cat.gif')"
+        document.body.style.backgroundSize = '5vw'
+        document.body.style.fontFamily = 'Comic Sans MS'
+        window.open('https://youtu.be/PkLkiLyySDU?si=ibM1-A1WdqZxdRjg')
+      }
+      if (input_password.value == 'lux') {
+        
+        window.alert('LIGHT')
+      }
+      if (input_password.value == 'sonechka') {
+        
+        window.open('poem.html')
+      }
+    }
+  });
+  document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+  });
