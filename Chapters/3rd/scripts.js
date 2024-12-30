d83d9ec986b0323ffab7d1e013da496090511200
Lux Sonechka <lux.sonechka@gmail.com>
2024-12-29 23:26:29 -0300

    Add files via upload

---
+++ Chapters/3rd/scripts.js
@@ -0,0 +1,3 @@
+function that_thing() {
+    window.open('https://www.canva.com/design/DAGS1nJ438o/XtuVOpna-i3WIhyrUa1RYA/edit?utm_content=DAGS1nJ438o&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton')
+}
